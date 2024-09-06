5d7eea6eefecb0e6eb317e611254cddd7feb88b7
Terry Aney <terry.aney@icloud.com>
2024-09-05 20:51:49 -0500

    Test push

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,8 @@
 from BreastCancerSet import BreastCancerSet
 from NaiveBayes import NaiveBayes
 
+# Test pushing to repository
+
 breast_cancer = BreastCancerSet()
 data = breast_cancer.get_data()
 naive = NaiveBayes()
