0ebcbc5fe7f152074b891fb5166c12c915844632
Josh Aney <josh.aney@icloud.com>
2024-09-06 10:49:30 -0600

    testing with seperate training and testing sets

--- src/Main.py
+++ src/Main.py
@@ -4,10 +4,14 @@ import numpy as np
 
 breast_cancer = BreastCancerSet()
 data = breast_cancer.get_data()
+train = data[:int(len(data) * .6)]
+test = data[-80:]
 labels = breast_cancer.get_labels()
+train_labels = labels[:int(len(data) * .6)]
+test_labels = labels[-80:]
 naive = NaiveBayes()
-naive.set_probabilities(data, labels)
-predictions = naive.classify(data)
+naive.set_probabilities(train, train_labels)
+predictions = naive.classify(test)
 
-print(np.mean(predictions == labels))
+print(np.mean(predictions == test_labels))
 
