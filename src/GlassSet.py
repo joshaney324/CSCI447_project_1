3f09d3ef9dd1a019f917090b542e7780bf1a930d
Owen <ohcool57@gmail.com>
2024-09-09 14:05:53 -0600

    Added binning to GlassSet - uses numpy builtin, but want to implement frequency-based in the future.

--- src/GlassSet.py
+++ src/GlassSet.py
@@ -7,8 +7,22 @@ class GlassSet:
     def __init__(self):
         with open("../data/glass.data", "r") as data_file:
             self.data = list(csv.reader(data_file, delimiter=','))
-        self.data = np.array(self.data)
-        self.data = self.data[:, 1:]
+        invalid_rows = []
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                try:
+                    self.data[i][j] = float(self.data[i][j])
+                except ValueError:
+                    invalid_rows.append(i)
+        self.data = np.array(self.data[:-1])
+        self.data = np.delete(self.data, 0, 1)
+        for i in range(len(self.data[0]) - 1):
+            column = self.data[:,i]
+            bins = np.histogram_bin_edges(column, bins = "auto")
+
+            bin_assignments = np.digitize(column, bins)
+            for j in range(len(column)):
+                self.data[j][i] = bin_assignments[j]
         np.random.shuffle(self.data)
 
     def get_data(self):
