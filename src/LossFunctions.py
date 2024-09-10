fixed accuracy bug, div by zero bug(recall/precision) cleaned up dictionaries

src/LossFunctions.py
@@ -7,19 +7,19 @@ def precision(predictions, labels):
     classes = np.unique(labels)
     class_precisions = []
     for class_instance in classes:
-        true_positives = 0
-        false_positives = 0
+        tp = 0
+        fp = 0
         for prediction, label in zip(predictions, labels):
             if prediction == class_instance and prediction == label:
-                true_positives += 1
+                tp += 1
             elif prediction == class_instance and prediction != label:
-                false_positives += 1
-        try:
-            class_precisions.append(float(true_positives/(true_positives + false_positives)))
-        except:
-            class_precisions.append(0.0)
-    output = list(zip(classes, class_precisions))
-    return output
+                fp += 1
+        if tp + fp == 0:
+            class_precisions.append(0)
+        else:
+            class_precisions.append(float(tp/(tp + fp)))
+
+    return list(zip(classes, class_precisions))
 
 
 def recall(predictions, labels):
@@ -28,38 +28,44 @@ def recall(predictions, labels):
     classes = np.unique(labels)
     class_recalls = []
     for class_instance in classes:
-        true_positives = 0
-        false_negatives = 0
+        tp = 0
+        fn = 0
         for prediction, label in zip(predictions, labels):
             if prediction == class_instance and prediction == label:
-                true_positives += 1
+                tp += 1
             elif prediction != class_instance and class_instance == label:
-                false_negatives += 1
-        class_recalls.append(float(true_positives / (true_positives + false_negatives)))
-    output = list(zip(classes, class_recalls))
-    return output
+                fn += 1
+        if tp + fn == 0:
+            class_recalls.append(0)
+        else:
+            class_recalls.append(float(tp / (tp + fn)))
+        
+    return list(zip(classes, class_recalls))
 
-#This function needs to be checked, but this implements accuracy as (TP+TN)/(TP+TN+FP+FN), rather than TP/(TP+TN+FP+FN)
+
+# This function needs to be checked, but this implements accuracy as (TP+TN)/(TP+TN+FP+FN), rather than TP/(TP+TN+FP+FN)
 def accuracy(predictions, labels):
     labels = np.array(labels)
     predictions = np.array(predictions)
     classes = np.unique(labels)
     class_accuracies = []
     for class_instance in classes:
-        true = 0
-        false = 0
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
         for prediction, label in zip(predictions, labels):
             if prediction == class_instance:
                 if prediction == label:
-                    true += 1
+                    tp += 1
                 else:
-                    false += 1
+                    fp += 1
             else:
                 if prediction == label:
-                    false += 1
+                    tn += 1
                 else:
-                    true += 1
-        class_accuracies.append(float(true / (true + false)))
-    output = list(zip(classes, class_accuracies))
-    return output
+                    fn += 1
+        class_accuracies.append(float((tp + tn) / (tp + tn + fp + fn)))
+        
+    return list(zip(classes, class_accuracies))
 

src/NaiveBayes.py
@@ -33,30 +33,24 @@ class NaiveBayes:
 
         # if class dictionary does not exist create a dictionary
         for class_instance in self.classes:
-            if class_instance not in self.attribute_probabilities:
-                self.attribute_probabilities[class_instance] = {}
-            if class_instance not in self.class_counts:
-                self.class_counts[class_instance] = 0
-
             # Get examples of the current class
             class_data = data[labels == class_instance]
+            self.attribute_probabilities[class_instance] = {}
             self.class_counts[class_instance] = len(class_data)
+
+            # get total number of class instances and then add d
+            total = len(class_data) + self.d
 
             for feature_idx in range(features):
                 # for each feature in the dataset, get every value that occurs for that feature as well as the counts
                 # for that specific value
                 # if dictionary key does not exist then create empy dictionary
-                if feature_idx not in self.attribute_probabilities[class_instance]:
-                    self.attribute_probabilities[class_instance][feature_idx] = {}
+                self.attribute_probabilities[class_instance][feature_idx] = {}
 
                 feature_values, value_counts = np.unique(class_data[:, feature_idx], return_counts=True)
-                # get total number of class instances and then add d
-                total = len(class_data) + self.d
                 # use zip function to combine all the unique values and their counts, then calculate the specific
                 # attribute probability
                 for value, count in zip(feature_values, value_counts):
-                    if value not in self.attribute_probabilities[class_instance][feature_idx]:
-                        self.attribute_probabilities[class_instance][feature_idx][value] = {}
                     self.attribute_probabilities[class_instance][feature_idx][value] = (count + 1) / total
 
     def calculate_total_probability(self, instance):